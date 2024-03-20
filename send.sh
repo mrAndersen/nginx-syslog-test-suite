a47cbe0c452462fd7d728993e613013a63f5bd10
alaverdyan.a <mrandersen2004@gmail.com>
2024-03-20 18:25:35 +0200

    test suite

--- send.sh
+++ send.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 message="<190>Jul 19 14:06:49 mrandersen-pc nginx: 127.0.0.1 [19/Jul/2019:17:10:49 +0300] \"GET /creative/1/ HTTP/1.1\" 204 0 \"-\" \"Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0\""
-echo -n "${message}" | nc -uc -w1 localhost 9222
+echo -n "${message}" | nc -uc 127.0.0.1 9222
